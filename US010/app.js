5720ec80c56e9f29c32ea1829512cdd464b978b7
Ahmeddzz <ahmedzahran95@hotmail.com>
2021-10-04 11:33:21 +0200

    enter customer details

--- US010/app.js
+++ US010/app.js
@@ -9,7 +9,18 @@ function showUserDetails() {
         alert("Customer ID must be less than 10 numbers!");
 
     } else {
-        tableCreate(customerID)
+
+        if (!!document.getElementById("dataTable")) {
+            document.getElementById('dataTable').remove();
+
+        }
+
+        tableCreate(customerID);
+
+
+
+
+
         return false;
     }
 
@@ -17,15 +28,20 @@ function showUserDetails() {
 
 
 function tableCreate(custId) {
+
+
     var body = document.body,
         tbl = document.createElement('table');
     var orderArrayHeader = ["Customer ID", " Call duration (sec)", "Data used (Kb)", "Date"];
+    tbl.setAttribute("id", "dataTable");
     tbl.style.width = '1000px';
     tbl.style.border = '1px solid black';
     tbl.style.marginLeft = 'auto';
     tbl.style.marginRight = 'auto';
     tbl.style.marginTop = '10px';
     tbl.style.padding = '10px';
+    tbl.style.textAlign = 'center';
+
     for (var i = 0; i < orderArrayHeader.length; i++) {
         tbl.appendChild(document.createElement("th")).
         appendChild(document.createTextNode(orderArrayHeader[i]));
@@ -63,8 +79,12 @@ function tableCreate(custId) {
     }
     body.appendChild(tbl);
 
+
 }
 
+function clear() {
+    document.getElementById("goy").innerHTML = "";
+}
 
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
@@ -80,3 +100,54 @@ var date_sort_desc = function(date1, date2) {
     if (date1 < date2) return 1;
     return 0;
 };
+
+
+
+// Enter usage details
+
+function showDiv() {
+    document.getElementById('welcomeDiv').style.display = "block";
+    return false;
+}
+
+
+function enterDetails() {
+    if (!!document.getElementById("dataTable")) {
+        document.getElementById('welcomeDiv').remove();
+
+    }
+    var customerId = document.getElementById('cust_id').value;
+    var callDuation = document.getElementById('call_duration').value;
+    var dataUsed = document.getElementById('data_used').value;
+    var date_ = document.getElementById('date').value;
+
+    if (isNaN(customerId)) {
+        alert("Customer ID must consist of numbers!");
+        return false;
+    } else if (customerId.length == 0) {
+        alert("Customer ID must be filled in!");
+        return false;
+    } else if (customerId.length > 10) {
+        alert("Customer ID must be less than 10 numbers!");
+        return false;
+
+    }
+
+    if (callDuation.length == 0) {
+        alert("Call duration must me filled in!");
+        return false;
+    }
+    if (dataUsed.length == 0) {
+        alert("Data usage must me filled in!");
+        return false;
+
+    }
+    if (date_.length == 0) {
+        alert("Date usage must me filled in!");
+        return false;
+    } else {
+        alert('Customer usage details saved!');
+    }
+
+    return false;
+}

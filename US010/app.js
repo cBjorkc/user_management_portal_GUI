0d84640f8693fb05ae62872546d6d88096edce02
Ahmeddzz <ahmedzahran95@hotmail.com>
2021-10-05 10:25:19 +0200

    us010 works again

--- US010/app.js
+++ US010/app.js
@@ -226,7 +226,7 @@ function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
-const d = randomDate(new Date(2012, 0, 1), new Date());
+
 
 
 var date_sort_desc = function(date1, date2) {
